b750f460f0a75afb98218da3d14dca4119859900
Bhuwan <bhubanpaudel79@gmail.com>
2023-12-26 09:42:43 +0545

    some change or improve to my website

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,72 +1,88 @@
-import React from 'react';
-import Typewriter from 'typewriter-effect';
+import React from "react";
+
+import Typewriter from "typewriter-effect";
+import { Link } from "react-scroll";
 function Main() {
   //for whatsapp number
   const handleWhatsAppClick = (phoneNumber) => {
-    const message = encodeURIComponent('Hi');
+    const message = encodeURIComponent("Hi");
     const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`;
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   };
-  const imageMy = 'https://live.staticflickr.com/65535/53301921526_f431ee79f9_w.jpg'
+  const imageMy =
+    "https://live.staticflickr.com/65535/53301921526_f431ee79f9_w.jpg";
   return (
-    <div className="main flex justify-between pt-10  md:pt-44 pb-96 md:pb-10  pl-[12%] pr-[23%] " >
-        {/* left  */}
-        <div className="left flex-initial text-text-color text-xl mt-2 ">
-          <div className="social grid gap-10">
-
-            <a href="https://twitter.com/BhuwanP69" target='_blank'>
-              <div className="div cursor-pointer hover:text-button-color hover:scale-110 transition-all text-2xl">
-            <iconify-icon icon="ri:twitter-line"></iconify-icon>
-              </div>
-            </a>
-            <a href="https://www.linkedin.com/in/bhuwan-paudel-591468224/
-            " target='_blank'>
-          <i class="fa-brands fa-linkedin cursor-pointer hover:text-button-color hover:scale-110 transition-all"></i>
-            </a>
-            <a href=""  onClick={() => handleWhatsAppClick('+977 9861658435')}>
-          <i class="fa-brands fa-whatsapp cursor-pointer hover:text-button-color hover:scale-110 transition-all"></i>
-            </a>
-            <a href="https://github.com/bhuwanP69" target='_blank'>
-          <i class="fa-brands fa-github cursor-pointer hover:text-button-color hover:scale-110 transition-all"></i>
-            </a>
-          </div>
-
-          {/* middle */}
+    <div className="main flex justify-between pt-10  md:pt-44 pb-96 md:pb-10  pl-[12%] pr-[23%] ">
+      {/* left  */}
+      <div className="left flex-initial text-text-color text-xl mt-2 ">
+        <div className="social grid gap-10">
+          <a href="https://twitter.com/BhuwanP69" target="_blank">
+            <div className="div cursor-pointer hover:text-button-color hover:scale-110 transition-all text-2xl">
+              <iconify-icon icon="ri:twitter-line"></iconify-icon>
+            </div>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/bhuwan-paudel-591468224/
+            "
+            target="_blank"
+          >
+            <i class="fa-brands fa-linkedin cursor-pointer hover:text-button-color hover:scale-110 transition-all"></i>
+          </a>
+          <a href="" onClick={() => handleWhatsAppClick("+977 9861658435")}>
+            <i class="fa-brands fa-whatsapp cursor-pointer hover:text-button-color hover:scale-110 transition-all"></i>
+          </a>
+          <a href="https://github.com/bhuwanP69" target="_blank">
+            <i class="fa-brands fa-github cursor-pointer hover:text-button-color hover:scale-110 transition-all"></i>
+          </a>
         </div>
-        <div className="middle absolute top-80 md:top-0 md:relative  grid gap-4 font-semibold w-80  ">
-          <h1 className=' text-[40px] md:text-[46px] font-semibold whitespace-nowrap text-black'>Hi, I'm Bhuwan</h1>
-          <h3 className='flex text-xl  md:text-2xl '>Creatve <span className='ml-1 text-text-color'> <Typewriter
-        options={{
-            strings: ['web developer.'],
-            autoStart: true,
-            loop: true,
-        }} 
-        /></span></h3>
-        <p className=' text-base md:text-lg'>I develop beautiful and responsive <span className='text-text-color'>SEO</span> optimized websites.</p>
-        <p className='text-base md:text-lg '>Want to take your <span className='text-text-color'>brand</span> to the next level?</p>
-        <button className='flex items-center justify-between bg-button-color w-28 px-3 font-semibold py-4 rounded-lg mb-24 mt-4 transition-all  hover:text-black' onClick={() => handleWhatsAppClick('+977 9861658435')}>
-          <p className='-mt-1 '>Hire Me </p>
+
+        {/* middle */}
+      </div>
+      <div className="middle absolute top-80 md:top-0 md:relative  grid gap-4 font-semibold w-80  ">
+        <h1 className=" text-[40px] md:text-[46px] font-semibold whitespace-nowrap text-black">
+          Hi, I'm Bhuwan
+        </h1>
+        <h3 className="flex text-xl  md:text-2xl ">
+          Creative{" "}
+          <span className="ml-1 text-text-color">
+            {" "}
+            <Typewriter
+              options={{
+                strings: ["Web Developer."],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </h3>
+        <p className=" text-base md:text-lg">
+          I’m a front-end developer specializing in the ReactJS ecosystem
+        </p>
+
+        <button
+          className="flex items-center justify-between bg-button-color w-36 px-3 font-semibold py-4 rounded-lg   transition-all  hover:text-black"
+          onClick={() => handleWhatsAppClick("+977 9861658435")}
+        >
+          <p className=" ">Contact Me </p>
           <div className="icon text-xl">
-          <iconify-icon icon="uil:message"></iconify-icon>
+            <iconify-icon icon="uil:message"></iconify-icon>
           </div>
-          
         </button>
-        <div className="scroll hidden md:flex items-center -mt-2 animate-bounce cursor-pointer transition-all">
-          <div className="mouse text-3xl text-text-color ">
-          <iconify-icon icon="mingcute:mouse-line"></iconify-icon>
-          </div>
-     
-            <p className='text-base font-semibold mr-2 -mt-2'>Scroll down</p>
+        <Link to="about" smooth>
+          <div className="scroll hidden md:flex items-center pt-10 animate-bounce cursor-pointer transition-all">
+            <div className="mouse text-3xl text-text-color ">
+              <iconify-icon icon="mingcute:mouse-line"></iconify-icon>
+            </div>
+
+            <p className="text-base font-semibold mr-2 -mt-2">Scroll down</p>
             <i class="fa-solid fa-arrow-down text-sm text-text-color"></i>
-         
-        </div>
-
-        </div>
-        {/* right  */}
-        <div className="right flex-initial mt-5">
-          <img src={imageMy} alt=""  className=' w-44 md:w-64 rounded-full'/>
-
-        </div>
+          </div>
+        </Link>
+      </div>
+      {/* right  */}
+      <div className="right flex-initial mt-5">
+        <img src={imageMy} alt="" className=" w-44 md:w-64 rounded-full" />
+      </div>
     </div>
   );
 }
--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,38 +1,53 @@
-
 export default function About() {
-        const imgMy = 'https://live.staticflickr.com/65535/53302405095_70002403b0_n.jpg'
+  const imgMy =
+    "https://live.staticflickr.com/65535/53420033456_e406327dc5_c.jpg";
   return (
-  <div className="about grid justify-center pt-16 " id='about'>
-    <div className="a-top flex flex-col items-center justify-center text-center ">
-    <h2 className='text-4xl font-bold text-black'>About Me</h2>
-    <p className='text-text-color'>Get to know me</p>
-    </div>
-    <div className="a-bottom mt-10 grid sm:flex justify-between pr-5 pl-20 md:pl-0">
+    <div className="about grid justify-center pt-16 " id="about">
+      <div className="a-top flex flex-col items-center justify-center text-center ">
+        <h2 className="text-4xl font-bold text-black">About Me</h2>
+        <p className="text-text-color">Get to know me</p>
+      </div>
+      <div className="a-bottom mt-10 grid sm:flex justify-between pr-5 pl-20 md:pl-0">
         <div className="a-left mr-[15%] pb-10 pl-10 md:pl-0">
-            <img src={imgMy} alt="img" className='w-52 md:w-full ' />
+          <img src={imgMy} alt="img" className="w-52 md:w-96 rounded-full " />
         </div>
         <div className="a-right  w-[360px]">
-            <p className='mb-5 text-[16px] '>With over 1+ years experience in building professional websites for businesses, brands and individuals, I bring you closer to your customers with my developer.</p>
-            <p className='text-[16px] '>Got a project? Hire me and enjoy quality & excellence in delivery.</p>
+          <p className="mb-5 text-[16px] ">
+            Hi! I'm Bhuwan Paudel, a software developer with a passion for
+            creating beautiful and functional websites. I have a strong
+            foundation in JavaScript, and am currently learning NextJS.
+          </p>
+          <p className="text-[16px] ">
+            Feel free to Contact me if you have any questions or just want to
+            chat!
+          </p>
 
-            <div className="numbers my-8 -ml-4 flex text-sm text-center">
-                <p className='w-24 '> <span className='block text-2xl font-semibold text-black'>01+</span> Years experience</p>
-                <p className='w-24'> <span className='block text-2xl font-semibold text-black'>5+</span> 
-                Completed Project</p>
-                <p className='w-24'> <span className='block text-2xl font-semibold text-black'>01+</span>Helped Companies</p>
-               
-            </div>
-            <a href="https://drive.google.com/file/d/1JV39rF3VF5r-J7T_CErzr_y1yk1iF7o6/view?usp=drive_link">
-            <button className='hover:bg-button-color bg-text-color font-sans font-semibold text-lg flex px-4 py-4 rounded-md transition-all ml-10 md:ml-0'>
-                <p className='mr-2 text-black'>Download CV</p>
-                <div className="icon font-bold text-xl text-black">
+          <div className="numbers my-8  flex gap-10 text-sm text-center">
+            <p className="w-24 ">
+              {" "}
+              <span className="block text-2xl font-semibold text-black">
+                01+
+              </span>{" "}
+              Years experience
+            </p>
+            <p className="w-24">
+              {" "}
+              <span className="block text-2xl font-semibold text-black">
+                5+
+              </span>
+              Completed Project
+            </p>
+          </div>
+          <a href="https://drive.google.com/file/d/1jpqlsi1fgmByeWj4YBdIlX8em4VXvwr3/view?usp=drive_link">
+            <button className="hover:bg-button-color bg-text-color font-sans font-semibold text-lg flex px-4 py-4 rounded-md transition-all ml-10 md:ml-0">
+              <p className="mr-2 text-black">Download CV</p>
+              <div className="icon font-bold text-xl text-black">
                 <iconify-icon icon="ion:download-outline"></iconify-icon>
-                </div>
+              </div>
             </button>
-            </a>
+          </a>
         </div>
-
+      </div>
     </div>
-  </div>
-  )
+  );
 }
